3aa69ba4d5e984cdbeb756b0cbbc197adabd5a6b
iamonuwa <matrix4u2002@gmail.com>
2020-02-02 02:38:32 +0100

    Use torus

--- dapp/src/App.vue
+++ dapp/src/App.vue
@@ -34,53 +34,38 @@
 </template>
 
 <script>
-// import Torus from "@toruslabs/torus-embed";
+import Torus from "@toruslabs/torus-embed";
 import Matic from "maticjs";
 import Web3 from "web3";
-import maticJSONData from "./testnet";
-// import { getCinemaAddress } from "./helpers/web3";
 export default {
   name: "App",
   data: () => ({
     isLoggedIn: false
-    // isAdmin: false
   }),
   methods: {
     login: async function() {
       try {
-        // const torus = new Torus({
-        //   buttonPosition: "bottom-left"
-        // });
-        // await torus.init({
-        //   buildEnv: "production",
-        //   enableLogging: true,
-        //   network: {
-        //     host: "https://testnet2.matic.network/",
-        //     networkName: "Matic Testnet v2"
-        //   },
-        //   showTorusButton: true
-        // });
-        // await torus.login();
-        // const web3 = new Web3(torus.provider);
-        let web3;
-        if (window.ethereum) {
-          web3 = new Web3(window.ethereum);
-          try {
-            // Request account access if needed
-            await window.ethereum.enable();
-          } catch (error) {
-            // User denied account access...
-          }
-        }
-        const testnetData = maticJSONData["TestnetV2"];
+        const torus = new Torus({
+          buttonPosition: "bottom-left"
+        });
+        await torus.init({
+          buildEnv: "production",
+          enableLogging: true,
+          network: {
+            host: "https://testnet2.matic.network/",
+            networkName: "Matic Testnet v2"
+          },
+          showTorusButton: true
+        });
+        await torus.login();
+        const web3 = new Web3(torus.provider);
         const maticObj = new Matic({
           maticProvider: web3.eth.currentProvider,
           parentProvider: web3.eth.currentProvider,
-          rootChainAddress: testnetData.Main.Contracts.RootChain
         });
         let accounts = await web3.eth.getAccounts();
         window.maticObj = maticObj;
-        // window.torus = torus;
+        window.torus = torus;
         window.web3 = web3;
         if (accounts[0]) {
           this.isLoggedIn = true;
